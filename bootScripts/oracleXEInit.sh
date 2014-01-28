61d544a42495b9fc4ce7d8045415b16da3d60178
jjo <jo6971@nododos.com>
2014-01-28 08:08:01 -0600

    initial add

---
+++ bootScripts/oracleXEInit.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#Amazon Linux AMI 2013.09.2 - ami-bba18dd2 (64 bit)
+#EBS volume /dev/sdf get blown away upon instance terminating
+
+#Akoya credentials
+AMI=ami-bba18dd2
+AWS_KEY=$1
+AWS_SECRET_KEY=$2
+
+ec2-run-instances $AMI --instance-count 1 --key "oracleEX_rsa" --aws-access-key $AWS_KEY --aws-secret-key $AWS_SECRET_KEY --group "OracleEx" --instance-type t1.micro --user-data-file ../initScripts/oracleXEBoot.sh --availability-zone us-east-1a --block-device-mapping  "/dev/sdf=:10" --instance-initiated-shutdown-behavior terminate 
