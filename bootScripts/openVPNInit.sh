b1c2701171d8e216687d06d0f910c4c8e49bbba8
Joe Olson <joe.olson@vgbio.com>
2014-02-09 12:46:36 -0600

    initial add

--- bootScripts/openVPNInit.sh
+++ bootScripts/openVPNInit.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #Amazon Linux AMI 2013.09.2 - ami-bba18dd2 (64 bit)
 
-AMI=ami-bba18dd2
+AMI=ami-81fe86e8
 AWS_KEY=$1
 AWS_SECRET_KEY=$2
 
-ec2-run-instances $AMI --instance-count 1 --key "openvpn_rsa" --network-attachment :0:subnet-87ae8cc1::10.0.4.10:sg-e473bf81:: --aws-access-key $AWS_KEY --aws-secret-key $AWS_SECRET_KEY --instance-type t1.micro --user-data-file ../initScripts/openVPNBoot.sh --availability-zone us-east-1d --instance-initiated-shutdown-behavior terminate 
+ec2-run-instances $AMI --instance-count 1 --key "openvpn_rsa" --network-attachment :0:subnet-87ae8cc1::10.0.4.10:sg-e473bf81:: --aws-access-key $AWS_KEY --aws-secret-key $AWS_SECRET_KEY --instance-type t1.micro --availability-zone us-east-1d --instance-initiated-shutdown-behavior stop
 
 
--- initScripts/openVPNBoot.sh
+++ initScripts/openVPNBoot.sh
@@ -1,23 +1,52 @@
 #!/bin/bash
 
-#create swap space
-dd if=/dev/zero of=/home/swapfile bs=1024 count=2097152
-mkswap /home/swapfile
-swapon /home/swapfile
-chown root:root /home/swapfile
-chmod 0600 /home/swapfile
-echo '/home/swapfile    swap      swap    defaults         0  0' >>/etc/fstab
-
 #directories and whatnot
-mkdir /home/ec2-user/downloads
-chown ec2-user:ec2-user /home/ec2-user/downloads
 
-#download stuff
-wget -O /home/ec2-user/downloads/epel-release-6-8.noarch.rpm http://dl.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm
-rpm -Uvh /home/ec2-user/downloads/epel-release-6-8.noarch.rpm
+
+#enable epl repo
+sed -i '0,/enabled=0/s//enabled=1/' /etc/yum.repos.d/epel.repo
 
 #install stuff
 yum update --assumeyes
 yum install openvpn --assumeyes
+yum install easy-rsa --assumeyes
+yum install mlocate --assumeyes
+
+#begin openvpn configuration
 cp /usr/share/doc/openvpn-*/sample/sample-config-files/server.conf /etc/openvpn
 
+echo '# Added by bootscript...' >> /etc/openvpn/server.conf
+echo 'push "redirect-gateway def1 bypass-dhcp"' >> /etc/openvpn/server.conf
+echo 'push "dhcp-option DNS 8.8.8.8"' >> /etc/openvpn/server.conf
+echo 'push "dhcp-option DNS 8.8.4.4"' >> /etc/openvpn/server.conf
+echo 'user nobody' >> /etc/openvpn/server.conf
+echo 'group nobody' >> /etc/openvpn/server.conf
+
+#set up certificates
+sed -i '0,/export KEY_PROVINCE="CA"/s//export KEY_PROVINCE="IL"/' /usr/share/easy-rsa/2.0/vars
+sed -i '0,/export KEY_CITY="SanFrancisco"/s//export KEY_CITY="Chicago"/' /usr/share/easy-rsa/2.0/vars
+sed -i '0,/export KEY_ORG="Fort-Funston"/s//export KEY_ORG="Orgname"/' /usr/share/easy-rsa/2.0/vars
+sed -i '0,/export KEY_EMAIL=mail@host.domain/s//export KEY_ORG=admin@domain.org/' /usr/share/easy-rsa/2.0/vars
+sed -i '0,/export KEY_EMAIL="mail@host.domain"/s//export KEY_ORG="admin@domain.org"/' /usr/share/easy-rsa/2.0/vars
+source /usr/share/easy-rsa/2.0/vars
+/usr/share/easy-rsa/2.0/clean-all
+#interaction starting here
+#/usr/share/easy-rsa/2.0/build-ca
+#and here
+#/usr/share/easy-rsa/2.0/build-key-server server
+#/usr/share/easy-rsa/2.0/build-dh
+
+#just pull the certs pre-generated from a repo?
+#cp /usr/share/easy-rsa/2.0/keys/dh1024.pem /etc/openvpn
+#cp /usr/share/easy-rsa/2.0/keys/ca.crt /etc/openvpn
+#cp /usr/share/easy-rsa/2.0/keys/server.crt /etc/openvpn
+#cp /usr/share/easy-rsa/2.0/keys/server.key /etc/openvpn
+
+#enable routing
+#iptables -t nat -A POSTROUTING -s 10.0.4.0/24 -o eth0 -j MASQUERADE
+#service iptables save
+#echo 'net.ipv4.ip_forward = 1' >>/etc/sysctl.conf
+#sed -i '0,/net.ipv4.ip_forward = 0/s//net.ipv4.ip_forward = 1/' /etc/sysctl.conf
+#sysctl -p
+#service openvpn start
+#chkconfig openvpn on
