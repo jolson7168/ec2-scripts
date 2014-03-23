eb841c7eb2ee1f45d31598efa62a799a7744dead
jjo <jo6971@nododos.com>
2014-03-23 10:39:44 -0500

    updated

--- bootScripts/featureGeneratorInit.sh
+++ bootScripts/featureGeneratorInit.sh
@@ -6,6 +6,6 @@ AWS_KEY=$1
 AWS_SECRET_KEY=$2
 NUM_INSTANCES=$3
 
-ec2-run-instances $AMI --instance-count NUM_INSTANCES --key "featureGenerator_rsa" --network-attachment :0:subnet-d8a0d4f0::10.0.10.55:sg-sg-d30ad6b6:: --aws-access-key $AWS_KEY --aws-secret-key $AWS_SECRET_KEY --instance-type m1.small --user-data-file ../initScripts/featureGeneratorBoot.sh --availability-zone us-east-1b --instance-initiated-shutdown-behavior terminate 
+ec2-run-instances $AMI --instance-count $NUM_INSTANCES --key "featureGenerator_rsa" --network-attachment :0:subnet-67645421:::sg-3e71d35b:: --aws-access-key $AWS_KEY --aws-secret-key $AWS_SECRET_KEY --instance-type m1.small --user-data-file ../initScripts/featureGeneratorBoot.sh --availability-zone us-east-1a --instance-initiated-shutdown-behavior terminate 
 
 
