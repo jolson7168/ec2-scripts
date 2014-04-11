ff23391deef7cd732292332bd0bf85ac60af8fe0
jjo <jo6971@nododos.com>
2014-04-11 11:49:13 -0500

    added new ones

--- bootScripts/postgresInit.sh
+++ bootScripts/postgresInit.sh
@@ -2,7 +2,7 @@
 #Amazon Linux AMI 2013.09.2 - ami-bba18dd2 (64 bit)
 #EBS volume /dev/sdf get blown away upon instance terminating
 
-AMI=ami-bba18dd2
+AMI=ami-2f726546
 AWS_KEY=$1
 AWS_SECRET_KEY=$2
 SUBNET=$3
@@ -14,7 +14,7 @@ TYPE=$7
 
 if [ "$TYPE" == "master" ] 
 then
-	ec2-run-instances $AMI --instance-count 1 --key "postgres_rsa" --network-attachment :0:$SUBNET::$IP:$SECURITY:: --aws-access-key $AWS_KEY --aws-secret-key $AWS_SECRET_KEY --instance-type m1.small --user-data-file ../initScripts/postgresBootMaster.sh --availability-zone $ZONE --block-device-mapping  "/dev/sdf=:10" --instance-initiated-shutdown-behavior terminate
+	ec2-run-instances $AMI --instance-count 1 --key "postgres_rsa" --network-attachment :0:$SUBNET::$IP:$SECURITY:: --aws-access-key $AWS_KEY --aws-secret-key $AWS_SECRET_KEY --instance-type m1.small --user-data-file ../initScripts/postgresBootMaster.sh --availability-zone $ZONE --block-device-mapping "/dev/sda1=:20":fals --instance-initiated-shutdown-behavior terminate
 else
-	ec2-run-instances $AMI --instance-count 1 --key "postgres_rsa" --network-attachment :0:$SUBNET::$IP:$SECURITY:: --aws-access-key $AWS_KEY --aws-secret-key $AWS_SECRET_KEY --instance-type m1.small --user-data-file ../initScripts/postgresBootSlave.sh --availability-zone $ZONE --block-device-mapping  "/dev/sdf=:10" --instance-initiated-shutdown-behavior terminate
+	ec2-run-instances $AMI --instance-count 1 --key "postgres_rsa" --network-attachment :0:$SUBNET::$IP:$SECURITY:: --aws-access-key $AWS_KEY --aws-secret-key $AWS_SECRET_KEY --instance-type m1.small --user-data-file ../initScripts/postgresBootSlave.sh --availability-zone $ZONE --block-device-mapping  "/dev/sda1=:20" --instance-initiated-shutdown-behavior terminate
 fi
--- bootScripts/redisInit.sh
+++ bootScripts/redisInit.sh
@@ -2,7 +2,7 @@
 #Amazon Linux AMI 2013.09.2 - ami-bba18dd2 (64 bit)
 #EBS volume /dev/sdf get blown away upon instance terminating
 
-AMI=ami-bba18dd2
+AMI=ami-2f726546
 AWS_KEY=$1
 AWS_SECRET_KEY=$2
 SUBNET=$3
@@ -15,6 +15,9 @@ TYPE=$7
 if [ "$TYPE" == "master" ] 
 then
 	ec2-run-instances $AMI --instance-count 1 --key "redis_rsa" --network-attachment :0:$SUBNET::$IP:$SECURITY:: --aws-access-key $AWS_KEY --aws-secret-key $AWS_SECRET_KEY --instance-type m1.small --user-data-file ../initScripts/redisBootMaster.sh --availability-zone $ZONE --block-device-mapping  "/dev/sdf=:10" --instance-initiated-shutdown-behavior terminate
+elif [ "$TYPE" == "standalone" ] 
+then
+	ec2-run-instances $AMI --instance-count 1 --key "redis_rsa" --network-attachment :0:$SUBNET::$IP:$SECURITY:: --aws-access-key $AWS_KEY --aws-secret-key $AWS_SECRET_KEY --instance-type m1.small --user-data-file ../initScripts/redisBoot.sh --availability-zone $ZONE --block-device-mapping  "/dev/sdf=:10" --instance-initiated-shutdown-behavior terminate
 else
 	ec2-run-instances $AMI --instance-count 1 --key "redis_rsa" --network-attachment :0:$SUBNET::$IP:$SECURITY:: --aws-access-key $AWS_KEY --aws-secret-key $AWS_SECRET_KEY --instance-type m1.small --user-data-file ../initScripts/redisBootSlave.sh --availability-zone $ZONE --block-device-mapping  "/dev/sdf=:10" --instance-initiated-shutdown-behavior terminate
  fi
