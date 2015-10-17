ff1305f149074964b34b62d10bd3250603420337
jolson7168 <technology@nododos.com>
2015-10-17 11:23:09 -0500

    needs heavy refactoring, but working enough to demo

--- src/pullFromQueue.py
+++ src/pullFromQueue.py
@@ -1,5 +1,5 @@
 from __future__ import print_function      # get latest print functionality
-
+#https://s3.amazonaws.com/cta-tracker/20150212.zip
 ###################################################
 # Python standard modules (libraries)
 
@@ -74,19 +74,21 @@ def unzipFile(fname):
 
 def toInteger(tmstmp):
     #20150211 23:58
-    return int(time.mktime(datetime.datetime.strptime(tmstmp, "%Y%m%d %H:%M").timetuple()))
+    if len(tmstmp) == 8:
+        mask = "%Y%m%d"
+    else:
+        mask = "%Y%m%d %H:%M"
+    return int(time.mktime(datetime.datetime.strptime(tmstmp, mask).timetuple()))
 
-def writeRiak(action, fix):
+def writeRiak(action, bucketname, key, data):
     global riak
 
-    #logger = logging.getLogger("pullfromQueue")
-    bucketname = str(fix["vid"])
-    key = str(toInteger(fix["tmstmp"]))
+    logger = logging.getLogger("pullfromQueue")
     bucket = riak.bucket(bucketname)
     if action == "write":
         obj = RiakObject(riak, bucket, key)
         obj.content_type = "application/json"
-        obj.data = fix
+        obj.data = data
         #index = None
         #if index is not None:
         #        for indexKey in index:
@@ -94,14 +96,14 @@ def writeRiak(action, fix):
         #                obj.add_index(indexKey,index[indexKey])
         #            except Exception as e:
          #               logger.error("Error updating index: "+e.message)
-        #startTime = time.time()
+        startTime = time.time()
         storedObj = obj.store()
-        #duration = round((time.time() - startTime),3)
-        #if storedObj is not None:
-        #    results = storedObj.key
-        #else:
-        #    results = "Not stored!"
-        #logger.info(" Write "+bucketname+"/"+key+" Sz: "+str(len(json.dumps(fix)))+" Dur: "+str(duration)+" Results: "+results)
+        duration = round((time.time() - startTime),3)
+        if storedObj is not None:
+            results = storedObj.key
+        else:
+            results = "Not stored!"
+        logger.info(" Write "+bucketname+"/"+key+" Sz: "+str(len(json.dumps(data)))+" Dur: "+str(duration)+" Results: "+results)
     elif action == "delete":
         got = bucket.get(key)
         startTime = time.time()
@@ -111,21 +113,41 @@ def writeRiak(action, fix):
     elif action == "test":
         print(key)
 
+def insertFix(fixes, newFix):
+    if newFix["vid"] not in fixes:
+        fixes[newFix["vid"]]=[]
+    fixes[newFix["vid"]].append(newFix)
+
+def writeRiakFixes(sortedFixes):
+    for fixTime in sortedFixes["sortedFixes"]:
+        if len(sortedFixes["sortedFixes"][fixTime]) > 0:
+            writeRiak("write",str(sortedFixes["vid"]),str(fixTime),sortedFixes["sortedFixes"][fixTime])
+
 def toRiakIndividual(fname):
+    fixes={}
     logger = logging.getLogger("pullfromQueue")
     startTime = time.time()
+    cnt=0
     with open(fname) as fixFile: 
         for fix in fixFile:
             if "vid" in fix:
-                writeRiak("write", json.loads(fix.replace("},","}")))
-                cnt = cnt + 1
-                if (cnt % 500) == 0:
-                    logger.info("Loaded "+str(cnt)+" fixes!")
+                try:
+                    for thisFix in fix.split("},"):
+                        if "{" in thisFix:  
+                            newFix = json.loads(thisFix+"}")
+                            newFix["fixTime"]=toInteger(newFix["tmstmp"])
+                            if newFix["vid"] not in fixes:
+                                fixes[newFix["vid"]]=[]
+                            fixes[newFix["vid"]].append(newFix)
+                            cnt = cnt + 1
+                except Exception as e:
+                    continue
+                if (cnt % 50000) == 0:
+                    logger.info("Prepared "+str(cnt)+" fixes")
+        logger.info("TOTAL: "+str(cnt)+" fixes!")                    
     duration = round((time.time() - startTime),3)   
-    logger.info("Loaded fix file individually. Duration: "+str(duration))
-    for fix in fixes["fixes"]:
-        writeRiak("write", fix)
-
+    logger.info("Prepared fixes individually. Duration: "+str(duration))
+    return fixes
 
 def toRiak(fname):
     logger = logging.getLogger("pullfromQueue")
@@ -147,7 +169,16 @@ def processFile(channel, method, properties, body):
     logger.info("Working: "+fileInfo["bucket"]+" "+fileInfo["filename"])
     thisFile = getFile(fileInfo["bucket"], fileInfo["filename"])
     unzipFile(thisFile)
-    toRiak(thisFile.replace(".zip",".json"))
+    #toRiak(thisFile.replace(".zip",".json"))
+    fixes = toRiakIndividual(thisFile.replace(".zip",".json"))
+    thisDate=(thisFile.replace(".zip","").replace("/tmp/",""))
+    intervals = getIntervals(thisDate)
+    for vid in fixes:
+        logger.info("Processing vid: "+str(vid))
+        sortedFixes = loadFixes(vid,sorted(fixes[vid], key=lambda fix: fix["fixTime"]),intervals)
+        writeRiakFixes(sortedFixes)
+
+
     channel.basic_ack(delivery_tag=method.delivery_tag)
 
 def configureMsgConsumer(server, queue_to_process, login, password, callback_func):
@@ -222,6 +253,42 @@ def initLog(rightNow):
     return logger
 
 
+def dumpFixes(vid, fixList):
+    print("VID: "+str(vid)+"----------------------------------------------")
+    for fix in fixList:
+        print("    "+str(fix["fixTime"]))
+
+def getInterval(fixtime, intervals):
+    #Is there a non-iterative way to do this? Probably...
+    retval = 0
+    for interval in intervals:
+        #print ("Interval: "+str(fixtime)+"  Checking: "+str(interval[0])+" and "+str(interval[1]))
+        if (fixtime>=interval[0]) and (fixtime<=interval[1]):
+            retval = interval[0]
+            break
+    #print("Found: "+str(retval))
+    return retval
+
+def loadFixes(vid, fixList,intervals):
+    retval={}
+    retval["vid"]=vid
+    retval["sortedFixes"]={}
+    for hour in intervals:
+        retval["sortedFixes"][hour[0]]=[]
+    for fix in fixList:
+        fixTime=fix["fixTime"]
+        whichInterval = getInterval(fixTime, intervals)
+        if whichInterval>0:
+            retval["sortedFixes"][whichInterval].append(fix)
+    return retval
+
+def getIntervals(theDate):
+    interval=[]
+    current=toInteger(theDate)
+    for hour in range(1,25):
+        interval.append((current,current+3600))
+        current=current+3600
+    return interval
 
 def main(argv):
     global riak 
@@ -249,10 +316,19 @@ def main(argv):
         elif opt in ("-q", "--queue"):
             queue=arg
 
-#    toRiakIndividual('/tmp/20150212.json')
     gConnection, gChannel = configureMsgConsumer(server, queue, login, password, processFile)
     gChannel.start_consuming()
     gConnection.close()
 
+
+
 if __name__ == "__main__":
     main(sys.argv[1:])
+
+        #print("===================================")  
+        #if vid == 800:
+        #    print("vid: "+str(vid))    
+        #    for fixTime in sortedFixes["sortedFixes"]:
+        #        print("   time: "+str(fixTime)+" Length: "+str(len(sortedFixes["sortedFixes"][fixTime])))
+        #        for eachFix in sortedFixes["sortedFixes"][fixTime]:
+        #            print("      fixtime: "+str(eachFix["fixTime"]))
