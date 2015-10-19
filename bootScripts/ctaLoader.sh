54738925a84a599dcbbe468da66606eabac92ab9
jolson7168 <technology@nododos.com>
2015-10-19 08:36:39 -0500

    interim fix until terraform scripts come on line

--- bootScripts/ctaLoader.sh
+++ bootScripts/ctaLoader.sh
@@ -2,12 +2,12 @@
 #default machine 4core, 15GB. Can run 4 threads (see below). But we will only run 3.
 echo "deb http://archive.ubuntu.com/ubuntu/ vivid universe" | sudo tee -a "/etc/apt/sources.list"
 sudo apt-get update
-sudo apt-get install python-pip
-sudo apt-get install python-dev libffi-dev libssl-dev
+sudo apt-get -y install python-pip
+sudo apt-get -y install python-dev libffi-dev libssl-dev
 sudo pip install cryptography
 sudo pip install pika
 sudo pip install riak
-git clone https://github.com/jolson7168/ec2-scripts.git /home/jjo31420/ec2-scripts
+#git clone https://github.com/jolson7168/ec2-scripts.git /home/jjo31420/ec2-scripts
 echo "python ../src/pullFromQueue.py --server 104.154.89.97 --login login --password password --queue cta" > /home/jjo31420/ec2-scripts/scripts/pullFromQueue.sh
 chmod +x /home/jjo31420/ec2-scripts/scripts/pullFromQueue.sh
 
@@ -18,6 +18,3 @@ chmod +x /home/jjo31420/ec2-scripts/scripts/pullFromQueue.sh
 #sleep 10
 #/home/jjo31420/ec2-scripts/scripts/pullFromQueue.sh &
 #sleep 10
-
-
-
